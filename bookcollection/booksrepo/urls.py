9661c184b5745d6ab1dd33dce0f2dde4f7a74459
karuna-sth <karunashrestha824@gmail.com>
2023-04-14 11:26:45 +0545

    Final Commit

--- bookcollection/booksrepo/urls.py
+++ bookcollection/booksrepo/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.booklist, name='booklist'),
     path('addbook/', views.addbook, name='addbook'),
-    path('deletebook/<id>', views.deletebook, name='deletebook')
+    path('deletebook/<id>', views.deletebook, name='deletebook'),
+    path('editbook/<id>', views.editbook, name='editbook'),
 ]
--- bookcollection/booksrepo/views.py
+++ bookcollection/booksrepo/views.py
@@ -24,6 +24,25 @@ def addbook(request):
     return render(request, 'addbooks.html', context={'page':'Add Book'})
 
 
+def editbook(request, id):
+    queryset = booksinfo.objects.get(id = id)
+    if request.method == 'POST':
+        data = request.POST
+        book_name = data.get('book_name')
+        book_writer = data.get('book_writer')
+        book_description = data.get('book_description')
+        image = request.FILES.get('image')
+        if image:
+            queryset.image = image
+        queryset.book_name = book_name
+        queryset.book_description = book_description
+        queryset.book_writer = book_writer
+        queryset.save()
+        return redirect("/")
+    context = {'page': 'Edit Book Details', 'book': queryset}
+    return render(request, "editbook.html", context)
+
+
 def deletebook(request, id):
     queryset = booksinfo.objects.get(id = id)
     queryset.delete()
